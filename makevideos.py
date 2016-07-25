cc6741c5f2be5a2daf41b4a472f8d4a562d58032
Brendan Coates <futuredaysmedia@gmail.com>
2016-07-25 08:36:23 -0700

    test build out

--- makevideos.py
+++ makevideos.py
@@ -6,6 +6,7 @@ import subprocess
 import sys
 import glob
 import re
+import argparse
 import ConfigParser
 from distutils import spawn
 
@@ -31,18 +32,22 @@ def dependencies():
 			sys.exit()
 	return
 
-def makelist():
+def makelist(rawCaptures):
 	try:
 		flist = []
+		for dirs, subdirs, files in os.walk(rawCaptures):
+			foo = blah
 		#walk through capture directory
 		#gather file names based on policy
 		#return dictionary of [endfilename : 'startfile1','startfile2','startfie3']
 	except:
+		foo = blah
 		#send email to THM staff
 	return flist
 
 def ffproces(flist):
 	try:
+		foo = blah
 		#iterate thru flist
 		#concatenate startfiles into endfile.mov
 		#transcode endfiles
@@ -50,11 +55,13 @@ def ffproces(flist):
 			#endfile.mpeg + timecode
 			#endfile.mp4 + timecode
 	except:
+		foo = blah
 		#send email to THM staff
 	return
 
 def hashmove(flist,sunnas,xendata,fmConfig):
 	try:
+		foo = blah
 		#iterate thru flist
 			#hashmove endfile.mov and endfile.mpeg to LTO
 				#send SHA1 hashes to FileMaker
@@ -63,10 +70,13 @@ def hashmove(flist,sunnas,xendata,fmConfig):
 			#hashmove endfile.flv to "SUNNAS/Digital Archive"
 				#send SHA1 hash to FileMaker
 	except:
+		foo = blah
+		
 		#send email to THM staff
 	return
 
 def playback(flist):
+	foo = blah
 	#iterate thru flist
 		#ffplay endfile.mov
 		#ffplay endfile.mpeg
@@ -77,29 +87,37 @@ def playback(flist):
 def main():
 	#initialize a buncha stuff
 	config = ConfigParser.ConfigParser()
-	config.read(s.path.join(os.cwd(),"video-post-process-config.txt"))
+	config.read(os.path.join(os.getcwd(),"video-post-process-config.txt"))
 	scriptRepo = config.get('global','scriptRepo')
 	watermark = config.get('transcode','watermark')
 	rawCaptures = config.get('transcode','rawCaptureDir')
-	fileMakerLoc = config.get('fileMaker','location')
-	fileMakerlogin = config.get('fileMaker','login')
-	fileMakerpwd = config.get('fileMaker','password')
+	#fileMakerLoc = config.get('fileMaker','location')
+	#fileMakerlogin = config.get('fileMaker','login')
+	#fileMakerpwd = config.get('fileMaker','password')
+	#fmConfig = [fileMakerLoc,fileMakerlogin,FileMakerpwd]
 	emailaddy = config.get('global','email')
 	sunnas = config.get('fileDestinations','sunnas')
 	xendata = config.get('fileDestinations','xendata')
-	fmConfig = [fileMakerLoc,fileMakerlogin,FileMakerpwd]
+	
+	#grab args fromCLI
+	parser = argparse.ArgumentParser(description="concatenates, transcodes, hashmvoes videos")
+	parser.add_argument("-i","--ignore",action="store_true",default=False,help="ignore policy errors")
+	parser.add_argument("-s","--single",help="single mode, only process a single accession takes full canonical filename as arg e.g.A2016_012_034_056")
+	
+	args = vars(parser.parse_args())
+	
 	
 	#make a list of things to work on
-	flist = makelist()
+	#flist = makelist(rawCaptures)
 
 	#ffprocess
-	ffprocess(flist,watermark)
+	#ffprocess(flist,watermark)
 
 	#hashmove
-	hashmove(flist,sunnas,xendata,fmConfig)
+	#hashmove(flist,sunnas,xendata)
 
 	#playback
-	playback(flist)
+	#playback(flist)
 
 	return
 
