5cf1d84406ae14c8f6a43af45740591a9fd55965
Brendan Coates <brendan.emerson.coates@gmail.com>
2025-09-22 10:12:33 -0700

    change scaling of derivative mp4s to 420:-1 - aka videos now maintain input aspect ratio @ 420px wide

--- transcodes.py
+++ transcodes.py
@@ -78,7 +78,7 @@ def make_mp4_with_tc(accession, file, kwargs):
         "\:00\:00\;00':r=29.97:x=(w-text_w)/2:y=(h-text_h)/1.2:fontcolor=white:fontsize=72:box=1:boxcolor=0x00000099"
     ffmpeg_cmd = 'ffmpeg -i ' + str(file) + \
         ' -c:v libx264 -b:v 372k -pix_fmt yuv420p -r 29.97 -vf ' +  yadif + drawtext + \
-        ',scale=420:270" -c:a aac -ar 44100 -ac 2 -map -0:d? -threads 0 -movflags +faststart -y ' + \
+        ',scale=420:-1" -c:a aac -ar 44100 -ac 2 -map -0:d? -threads 0 -movflags +faststart -y ' + \
         str(mp4_fullpath) + kwargs.ffmpeg_suffix
     ffmpeg_ok = run_ffmpeg(ffmpeg_cmd)
     if not ffmpeg_ok:
@@ -98,7 +98,7 @@ def make_mp4_with_logo(accession, file, kwargs):
     else:
         yadif = ""
     ffmpeg_cmd = 'ffmpeg -i ' + str(file) + ' -i ' + str(kwargs.watermark_white) + \
-        ' -filter_complex ' + yadif + 'overlay=0:0,scale=420:270 ' \
+        ' -filter_complex ' + yadif + 'overlay=0:0,scale=420:-1 ' \
         + '-c:v libx264 -b:v 372k -pix_fmt yuv420p -r 29.97 -c:a aac -ar 44100 -ac 2 -map -0:d? -threads 0 -movflags +faststart -y ' \
         + str(mp4_fullpath) + kwargs.ffmpeg_suffix
     ffmpeg_ok = run_ffmpeg(ffmpeg_cmd)
